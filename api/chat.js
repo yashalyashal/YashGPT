3252801f052a4fef2c20916adf440527060cb2a6
yashalyashal <yashalareef1071@gmail.com>
2026-03-16 07:54:57 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   const { parts } = req.body;
 
   try {
-    const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-04-17:generateContent?key=${apiKey}`;
     
     const response = await fetch(url, {
       method: 'POST',
